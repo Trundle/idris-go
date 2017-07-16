5a31529fdd9c824ba659f68f4d9703fe93fa1cb0
Andreas Stührk <andy@hammerhartes.de>
2017-07-16 15:27:24 +0200

    Dead-end: use the bytecode

---
+++ src/Main.hs
@@ -0,0 +1,47 @@
+module Main where
+
+import Idris.AbsSyntax
+import Idris.Core.TT
+import Idris.ElabDecls
+import Idris.Main
+import IRTS.CodegenGo
+import IRTS.Compiler
+
+import Util.System
+
+import Paths_idris_go
+
+import Control.Monad
+import System.Environment
+import System.Exit
+
+
+
+data Opts = Opts { inputs :: [FilePath],
+                   output :: FilePath }
+
+showUsage = do putStrLn "A code generator which is intended to be called by the compiler, not by a user."
+               putStrLn "Usage: idris-codegen-c <ibc-files> [-o <output-file>]"
+               exitSuccess
+
+getOpts :: IO Opts
+getOpts = do xs <- getArgs
+             return $ process (Opts [] "a.out") xs
+  where
+    process opts ("-o":o:xs) = process (opts { output = o }) xs
+    process opts (x:xs) = process (opts { inputs = x:inputs opts }) xs
+    process opts [] = opts
+
+c_main :: Opts -> Idris ()
+c_main opts = do runIO setupBundledCC
+                 elabPrims
+                 loadInputs (inputs opts) Nothing
+                 mainProg <- elabMain
+                 ir <- compile (Via IBCFormat "go") (output opts) (Just mainProg)
+                 runIO $ codegenGo ir
+
+main :: IO ()
+main = do opts <- getOpts
+          if (null (inputs opts))
+             then showUsage
+             else  runMain (c_main opts)
