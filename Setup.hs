54a602ac4c5053368de489bc869f31736ea8bd6e
Andreas Stührk <andy@hammerhartes.de>
2017-08-01 22:07:05 +0200

    Enable Travis CI and make it work with latest Idris
    Unfortunately we run into either a timeout or a memory limit with Travis
    CI, so it's a bit useless right now.

--- Setup.hs
+++ Setup.hs
@@ -1,19 +1,21 @@
-import qualified Distribution.Simple.Program as P
-import qualified Distribution.Simple.Setup as S
+import qualified Distribution.Simple.Program     as P
+import qualified Distribution.Simple.Setup       as S
 
-import Distribution.Simple
+import           Distribution.Simple
 
 make verbosity = P.runProgramInvocation verbosity . P.simpleProgramInvocation "make"
 buildGoLibs _ flags _ _ = do
   let verbosity = S.fromFlag $ S.buildVerbosity flags
   putStrLn "Building libraries..."
   make verbosity ["-C", "libs", "build" ]
-installGoLibs _ flags _ _ = do
-  let verbosity = S.fromFlag $ S.installVerbosity flags
+installGoLibs verbosity = do
   putStrLn "Installing libraries..."
   make verbosity ["-C", "libs", "install" ]
 
 main = defaultMainWithHooks $ simpleUserHooks
   { postBuild = buildGoLibs
-  , postInst = installGoLibs
+  , postCopy = \_ flags pkg local ->
+                 installGoLibs (S.fromFlag $ S.copyVerbosity flags)
+  , postInst = \_ flags pkg local ->
+                  installGoLibs (S.fromFlag $ S.installVerbosity flags)
   }
